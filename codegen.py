eb1aa66761c30cdb15b6f679277242b43e487a7e
subharatti01 <rattis4@mcmaster.ca>
2026-02-13 16:12:46 -0500

    Getting entities and relationships from cells using Python XML library

--- codegen.py
+++ codegen.py
@@ -18,3 +18,32 @@ root = tree.getroot() #parsing xml
 things = {}
 relationships = []
 specializations = [] #hold translation semantic results
+
+for cell in root.iter("mxCell"): #available in the xml element tree library, so go thru every cell
+
+    vertex = cell.get("vertex")
+    edge = cell.get("edge")
+
+    if vertex == "1": #finds the entities (rectangles)
+        id_value = cell.get("id")
+        name_value = cell.get("value")
+
+        if name_value is not None and name_value != "":
+            things[id_value] = name_value
+
+    if edge == "1": #find the relations (arrows)
+        source = cell.get("source")
+        target = cell.get("target")
+        label = cell.get("value")
+        style = cell.get("style")
+
+        if label is None:
+            label = ""
+
+        if style is None:
+            style = ""
+
+        if "endArrow=block" in style: #checks if its inheritance
+            specializations.append([source, target])
+        else:
+            relationships.append([source, target, label])
