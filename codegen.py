466b821eebcebaed4b74b884c87576588ddb21ff
subharatti01 <rattis4@mcmaster.ca>
2026-02-13 15:52:10 -0500

    Checking arguments and parse file

--- codegen.py
+++ codegen.py
@@ -1,1 +1,20 @@
-#code
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+if len(sys.argv) != 3:
+    print("give input file and output folder") #folder not given
+    sys.exit()
+
+input_file = sys.argv[1]
+output_folder = sys.argv[2] #storing the input and output folders for import and export
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder) #making output folder incase
+
+tree = ET.parse(input_file)
+root = tree.getroot() #parsing xml
+
+things = {}
+relationships = []
+specializations = []
